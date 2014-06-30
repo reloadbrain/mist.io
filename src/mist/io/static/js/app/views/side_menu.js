a44f9feb93b1766858f57463746403e48bf29f48
George Tsopanoglou <youtsop@gmail.com>
2014-07-01 02:43:13 +0300

    Add side menu view

---
+++ src/mist/io/static/js/app/views/side_menu.js
@@ -0,0 +1,16 @@
+define('app/views/side_menu', ['app/views/panel'],
+    //
+    //  Side Menu View
+    //
+    //  @returns Class
+    //
+    function (PanelView) {
+
+        'use strict';
+
+        return PanelView.extend({
+
+
+        });
+    }
+);
